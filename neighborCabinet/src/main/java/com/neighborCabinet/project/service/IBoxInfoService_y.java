69978681cf4d76badf6abc21ff01bf10ab12212b
YuJuHyun <wngus1997@naver.com>
2023-01-12 17:16:43 +0900

    헤더 수정, boxOrder 관련 백엔드, 이미지 추가

---
+++ neighborCabinet/src/main/java/com/neighborCabinet/project/service/IBoxInfoService_y.java
@@ -0,0 +1,10 @@
+package com.neighborCabinet.project.service;
+
+import java.util.ArrayList;
+
+import com.neighborCabinet.project.model.BoxInfoVO_y;
+
+public interface IBoxInfoService_y {
+	public ArrayList<BoxInfoVO_y> type_AList();
+	public ArrayList<BoxInfoVO_y> type_BList();
+}
---
+++ neighborCabinet/src/main/resources/static/js/boxOrder.js
@@ -0,0 +1,15 @@
+/**
+ * header.js
+ */
+
+$(document).ready(function(){
+	
+	$('.select_checkBox').on('click', function(){
+		var check = $('.select_checkBox').is(':checked');
+		var orderList = $('#box_QtyBox');
+		if(check){
+		} else{
+		}
+	});
+
+});

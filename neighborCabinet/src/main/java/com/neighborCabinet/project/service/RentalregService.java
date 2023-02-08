1b20daf57e3993acf8d3d67fc426def07419a4de
shkimhi <ajfqhksh1@gmail.com>
2023-02-08 17:42:18 +0900

    등록 페이지 박스 타입 DB 연동 , 디테일페이지 사진 출력 , 픽업/음식 수정

--- neighborCabinet/src/main/java/com/neighborCabinet/project/service/RentalregService.java
+++ neighborCabinet/src/main/java/com/neighborCabinet/project/service/RentalregService.java
@@ -3,6 +3,7 @@ package com.neighborCabinet.project.service;
 import com.neighborCabinet.project.dao.IRentalregDAO;
 import com.neighborCabinet.project.model.BoxVO;
 import com.neighborCabinet.project.model.PlaceInfoVO;
+import com.neighborCabinet.project.model.boxtypeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,16 @@ public class RentalregService implements IRentalService {
     public ArrayList<BoxVO> boxreg(String userId) {
         return reg.boxreg(userId);
     }
+    @Override
+    public int boxPreg(boxtypeVO boxtype) {
+        return reg.boxPreg(boxtype);
+    }
+
+    @Override
+    public int boxDate(boxtypeVO boxtype) {
+        return reg.boxDate(boxtype);
+    }
+
 
 
 }

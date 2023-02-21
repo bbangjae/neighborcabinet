b658d546e8649ad4fcde782064a7ea6cdeb1ee9a
bbangjae <120161896+bbangjae@users.noreply.github.com>
2023-02-21 17:35:11 +0900

    Update MypageController.java

--- neighborCabinet/src/main/java/com/neighborCabinet/project/controller/MypageController.java
+++ neighborCabinet/src/main/java/com/neighborCabinet/project/controller/MypageController.java
@@ -35,148 +35,149 @@ import com.neighborCabinet.project.service.RentHistoryService;
 
 @Controller
 public class MypageController {
-	@Autowired
-	private IUsermodifyService iUsermodifyService;
+   @Autowired
+   private IUsermodifyService iUsermodifyService;
 
-	@RequestMapping("/mypage")
-	public String index() {
-		return "mypage/MyPage";
-	}
+   @RequestMapping("/mypage")
+   public String index() {
+      return "mypage/MyPage";
+   }
 
-	@GetMapping("/modifyInfo")
-	public String memberGetDetailGet(Model model, HttpSession session) throws Exception {
-		String userId = (String) session.getAttribute("sid");
-		model.addAttribute("memberInfo", iUsermodifyService.memberGetDetail(userId));
-		return "mypage/modifyInfo";
-	}
+   @GetMapping("/modifyInfo")
+   public String memberGetDetailGet(Model model, HttpSession session) throws Exception {
+      String userId = (String) session.getAttribute("sid");
+      model.addAttribute("memberInfo", iUsermodifyService.memberGetDetail(userId));
+      return "mypage/modifyInfo";
+   }
 
-	@GetMapping("/modifyInfo2")
-	public String memberGetDetailGet1(Model model, HttpSession session) throws Exception {
-		String userId = (String) session.getAttribute("sid");
-		model.addAttribute("memberInfo", iUsermodifyService.memberGetDetail(userId));
-		return "mypage/modifyInfo2";
-	}
+   @GetMapping("/modifyInfo2")
+   public String memberGetDetailGet1(Model model, HttpSession session) throws Exception {
+      String userId = (String) session.getAttribute("sid");
+      model.addAttribute("memberInfo", iUsermodifyService.memberGetDetail(userId));
+      return "mypage/modifyInfo2";
+   }
 
-	@RequestMapping("/modifyInfo!")
-	public String memberModifyPOST(UsermodifyVO member, RedirectAttributes rttr) throws Exception {
-		int result = iUsermodifyService.memberModify(member);
-		rttr.addFlashAttribute("modify_result", result);
-		return "redirect:/modifyInfo";
-	}
+   @RequestMapping("/modifyInfo!")
+   public String memberModifyPOST(UsermodifyVO member, RedirectAttributes rttr) throws Exception {
+      int result = iUsermodifyService.memberModify(member);
+      rttr.addFlashAttribute("modify_result", result);
+      return "redirect:/modifyInfo";
+   }
 
-	@PostMapping("/deleteInfo")
-	public String memberDeletePOST(String userId, RedirectAttributes rttr) {
+   @PostMapping("/deleteInfo")
+   public String memberDeletePOST(String userId, RedirectAttributes rttr) {
 
-		int result = 0;
+      int result = 0;
 
-		try {
+      try {
 
-			result = iUsermodifyService.memberDelete(userId);
+         result = iUsermodifyService.memberDelete(userId);
 
-		} catch (Exception e) {
+      } catch (Exception e) {
 
-			e.printStackTrace();
-			result = 2;
-			rttr.addFlashAttribute("delete_result", result);
+         e.printStackTrace();
+         result = 2;
+         rttr.addFlashAttribute("delete_result", result);
 
-			return "redirect:/";
+         return "redirect:/";
 
-		}
+      }
 
-		rttr.addFlashAttribute("delete_result", result);
+      rttr.addFlashAttribute("delete_result", result);
 
-		return "redirect:/";
+      return "redirect:/";
 
-	}
+   }
 
-	@Autowired
-	IBoxmodifyService iBoxmodifyService;
+   @Autowired
+   IBoxmodifyService iBoxmodifyService;
 
-	@Autowired
-	RentHistoryService service;
+   @Autowired
+   RentHistoryService service;
 
-	@Autowired
-	LDealHistoryService LdealService;
+   @Autowired
+   LDealHistoryService LdealService;
 
-	@Autowired
-	BoxhistoryService BoxService;
+   @Autowired
+   BoxhistoryService BoxService;
 
-	@Autowired
-	IQrconfirmService QRService;
+   @Autowired
+   IQrconfirmService QRService;
 
-	@RequestMapping("/lenderPage")
-	public String rental(@ModelAttribute("searchVO") RentHistoryVO searchVO,
-			@ModelAttribute("boxVO") boxHistoryVO boxVO, BoxmodifyVO boxmodify, Model model, HttpSession session)
-			throws Exception {
-		String userId = (String) session.getAttribute("sid");
+   @RequestMapping("/lenderPage")
+   public String rental(@ModelAttribute("searchVO") RentHistoryVO searchVO,
+         @ModelAttribute("boxVO") boxHistoryVO boxVO, BoxmodifyVO boxmodify, Model model, HttpSession session)
+         throws Exception {
+      String userId = (String) session.getAttribute("sid");
 
-		// 박스 불러오기
-		model.addAttribute("boxStock", iBoxmodifyService.boxStock(userId));
+      // 박스 불러오기
+      model.addAttribute("boxStock", iBoxmodifyService.boxStock(userId));
 
-		// 박스 구매 내역
-		ArrayList<boxHistoryVO> boxAllHistory = BoxService.listBoxHistory(userId);
-		model.addAttribute("boxAllHistory", boxAllHistory);
+      // 박스 구매 내역
+      ArrayList<boxHistoryVO> boxAllHistory = BoxService.listBoxHistory(userId);
+      model.addAttribute("boxAllHistory", boxAllHistory);
 
-		// 거래 내역
-		ArrayList<LDealHistoryVO> LdealAllHistory = LdealService.LlistAllDeal(userId);
-		model.addAttribute("LdealAllHistory", LdealAllHistory);
+      // 거래 내역
+      ArrayList<LDealHistoryVO> LdealAllHistory = LdealService.LlistAllDeal(userId);
+      model.addAttribute("LdealAllHistory", LdealAllHistory);
 
-		// 대여 내역
-		ArrayList<RentHistoryVO> rentAllHistory = service.listAllHistory(searchVO);
-		model.addAttribute("rentAllHistory", rentAllHistory);
+      // 대여 내역
+      ArrayList<RentHistoryVO> rentAllHistory = service.listAllHistory(searchVO);
+      model.addAttribute("rentAllHistory", rentAllHistory);
 
-		return "mypage/lenderPage";
-	}
+      return "mypage/lenderPage";
+   }
 
-	@RequestMapping("/modifyBox")
-	public String boxModifyPOST(BoxmodifyVO boxmodify, HttpSession session) throws Exception {
-		String userId = (String) session.getAttribute("sid");
-		boxmodify.setUserId(userId);
-		iBoxmodifyService.boxModify(boxmodify);
+   @RequestMapping("/modifyBox")
+   public String boxModifyPOST(BoxmodifyVO boxmodify, HttpSession session) throws Exception {
+      String userId = (String) session.getAttribute("sid");
+      boxmodify.setUserId(userId);
+      iBoxmodifyService.boxModify(boxmodify);
 
-		return "redirect:/lenderPage";
-	}
+      return "redirect:/lenderPage";
+   }
 
-	@RequestMapping("/modifyBox2")
-	public String boxModifyPOST2(BoxmodifyVO boxmodify, HttpSession session) throws Exception {
-		String userId = (String) session.getAttribute("sid");
-		boxmodify.setUserId(userId);
-		iBoxmodifyService.boxModify2(boxmodify);
+   @RequestMapping("/modifyBox2")
+   public String boxModifyPOST2(BoxmodifyVO boxmodify, HttpSession session) throws Exception {
+      String userId = (String) session.getAttribute("sid");
+      boxmodify.setUserId(userId);
+      iBoxmodifyService.boxModify2(boxmodify);
 
-		return "redirect:/lenderPage";
-	}
+      return "redirect:/lenderPage";
+   }
 
-	@RequestMapping("/guidePage")
-	public String dealNotice(Model model) {
+   @RequestMapping("/guidePage")
+   public String dealNotice(Model model) {
 
-		return "mypage/guidePage";
-	}
+      return "mypage/guidePage";
+   }
 
-	/*
-	 * @ResponseBody
-	 * 
-	 * @RequestMapping("/QRconfirm") public String QRconfirmPOST(@RequestParam
-	 * HashMap<String, Object> param, HttpSession session) throws Exception { String
-	 * userId = (String) session.getAttribute("sid"); Reserve.setSender(userId);
-	 * QRService.QRconfirm(param);
-	 * 
-	 * return "success"; }
-	 */
+   /*
+    * @ResponseBody
+    * 
+    * @RequestMapping("/QRconfirm") public String QRconfirmPOST(@RequestParam
+    * HashMap<String, Object> param, HttpSession session) throws Exception { String
+    * userId = (String) session.getAttribute("sid"); Reserve.setSender(userId);
+    * QRService.QRconfirm(param);
+    * 
+    * return "success"; }
+    */
 
-	/*
-	 * @ResponseBody
-	 * 
-	 * @RequestMapping("/dealFinish") public String dealFinishPOST(@RequestParam
-	 * HashMap<String, Object> param, HttpSession session)throws Exception{ String
-	 * userId = (String) session.getAttribute("sid"); Reserve.setSender(userId);
-	 * QRService.dealFinish(param);
-	 * 
-	 * return "success"; }
-	 */
+   
+     @ResponseBody
+     @RequestMapping("/dealFinish") 
+     public String dealFinishPOST(@RequestParam HashMap<String, Object> param, HttpSession session)throws Exception{ 
+     String userId = (String) session.getAttribute("sid"); 
+      /* Reserve.setSender(userId); */
+     QRService.dealFinish(param);
+     
+     return "success";
+     }
 
-	@RequestMapping("/boxUpdate")
-	public String boxUpdate() {
 
-		return "mypage/boxUpdate";
-	}
+   @RequestMapping("/boxUpdate")
+   public String boxUpdate() {
+
+      return "mypage/boxUpdate";
+   }
 }
